0d8a1ff0bfb93727e1de3889ec485189acae3bc7
Mimi1101 <113716446+Mimi1101@users.noreply.github.com>
2023-11-17 13:54:26 -0600

    Add files via upload
    I need to add more code in this but i have only defined the intial structs we need

---
+++ PA4 Operating Systems/filesystem.c
@@ -0,0 +1,49 @@
+# include <stdio.h>;
+#include "filesystem.h"
+#include "softwaredisk.h"
+#include "softwaredisk.c"
+#include <stdint.h>
+
+
+//
+// Created By mimic on 11/15/2023.
+//
+
+
+
+typedef struct Inode{
+
+} Inode;
+
+typedef struct InodeBlock {
+ 
+}InodeBlock;
+
+typedef struct InodeDirectBlock{
+    
+}InodeDirectBlock;
+
+typedef struct DirEntry{
+
+}DirEntry;
+
+typedef struct DirEntryBlock{
+
+}DirEntryBlock;
+
+typedef struct FreeBitMap{
+
+}FreeBitMap;
+
+//basically FileInternal
+typedef struct Files{
+
+}Files
+
+// uint8_t njxk = [4096]
+
+
+
+
+
+2
